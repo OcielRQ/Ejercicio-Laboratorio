1a9429076e0578b030b3ddd6e4bb7475f1c4ddc4
Ociel <Ociel-raq@hotmail.com>
2018-08-31 04:57:42 -0500

    Laboratorio

--- Laboratorio.java
+++ Laboratorio.java
@@ -8,4 +8,25 @@ public class Laboratorio
     {
         alumnos = new ArrayList<String>();
     }
+    
+    public void ingresaAlumno(String alumno, String clave)
+    {
+        String alu;
+        alu = alumno + " " + clave;
+        alumnos.add(alu);
+    }
+    
+    public void darBaja(String alumno, String clave)
+    {
+       String alu;
+       alu = alumno + " " + clave;
+       alumnos.add(alu); 
+       for(int i = 0; i < alumnos.size(); i++)
+       {
+           String aux = alumnos.get(i);
+           if(aux == alu){
+               alumnos.remove(i);
+           }
+       }
+    }
 }
